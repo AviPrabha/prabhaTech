db495ceb9aa22c8d28e3ba02b0ee7eb2bd136fb1
AviPrabha <58522761+AviPrabha@users.noreply.github.com>
2019-12-05 20:41:51 +0530

    project Created

---
+++ src/Company.java
@@ -0,0 +1,16 @@
+class Company
+{
+	static String companyName;
+	String employeeName;
+	int employeeId;
+	static double companyLogInTime;
+
+static void getofficeTime(Company naam)
+{
+	System.out.println(naam.companyName);
+}
+void work()
+{
+	System.out.println("employeeName is " +employeeName + " employeeId is " +employeeId);
+}
+}
---
+++ src/Employee.java
@@ -0,0 +1,18 @@
+class Employee
+{
+	public static void main(String[] args) {
+		Company c1=new Company();
+		Company c2=new Company();
+		Company.companyName="google";
+		Company.companyName();
+		/*Company.companyLogInTime=9.15;
+		c1.employeeName="Avinash";
+		c1.employeeId=124421;
+		c2.employeeName="HemaMalini";
+		c2.employeeId=421124;
+		c1.work();
+		c2.work();
+		c1.getofficeTime();
+		c2.getofficeTime();*/
+	}
+}
